9d765f42b70f5d98979821598b3d58bd906b5139
herry88 <herry081288@gmail.com>
2019-04-24 10:47:55 +0700

    Baru Lagi

--- rowflutter/lib/ListStatis_page.dart
+++ rowflutter/lib/ListStatis_page.dart
@@ -2,17 +2,37 @@ import 'package:flutter/material.dart';
 // import 'package:rowflutter/index.dart';
 
 class ListStatisPage extends StatelessWidget {
-  static const String routeName = "/listStatis";
+  // static const String routeName = "/listStatis";
 
   @override
   Widget build(BuildContext context) {
-    // var _listStatisBloc = new ListStatisBloc();
+    // var _layoutPageBloc = new LayoutPageBloc();
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Lists Statis"),
-        backgroundColor: Colors.grey,
+        title: new Text("LayoutPage"),
       ),
-      // body: new ListStatisScreen(listStatisBloc: _listStatisBloc),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Mail'),
+           
+          ),
+          ListTile(
+            leading: Icon(Icons.map),
+            title: Text('Map'),
+          ),
+          ListTile(
+            leading: Icon(Icons.album),
+            title: Text('Album'),
+          ),
+          ListTile(
+            leading: Icon(Icons.alarm),
+            title: Text('Alarm'),
+          ),
+        ],
+      ),
+      // body: new LayoutPageScreen(layoutPageBloc: _layoutPageBloc),
     );
   }
 }
