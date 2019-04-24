713d5c8538c2c1fe70396c2a2e9edd4a31fb046c
herry88 <herry081288@gmail.com>
2019-04-24 13:14:11 +0700

    BaruLagi

--- rowflutter/lib/ListsDinamisPage_page.dart
+++ rowflutter/lib/ListsDinamisPage_page.dart
@@ -42,13 +42,45 @@ class TodoScreen extends StatelessWidget {
             return Padding(
               padding: const EdgeInsets.all(5.0),
               child: Card(
-                  child: ListTile(
+                child: ListTile(
                   title: Text(todos[index].judul),
                   subtitle: Text(todos[index].detail),
+                  onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          DetailScreen(todo: todos[index]))),
                 ),
               ),
             );
           }),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final Todo todo;
+  DetailScreen({Key key, @required this.todo}) : super(key: key);
+  // const DetailScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.judul),
+        backgroundColor: Colors.green,
+      ),
+      body: Column(
+        children: <Widget>[
+          new Text(
+            todo.judul,
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.justify,
+          ),
+          new Text(
+            todo.detail,
+            textAlign: TextAlign.justify,
+          )
+        ],
+      ),
     );
   }
 }
