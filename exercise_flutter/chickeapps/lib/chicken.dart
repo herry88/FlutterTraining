00a1ff9b2f0d7b62bb64254ef9b53cff91060f91
herry88 <herry081288@gmail.com>
2019-04-26 13:53:54 +0700

    UpdateLagiBro

---
+++ exercise_flutter/chickeapps/lib/chicken.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class Chicken{
+  final int id;
+  final String title;
+  final String word;
+  final double rating;
+  final String alcohol;
+
+}
